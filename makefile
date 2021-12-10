b93ed18f55d4b85fc8ab3374f6d969f907f54d97
Maxwell Zen <maxwell.zen@gmail.com>
2021-12-10 01:12:23 -0500

    did assignment

---
+++ makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -o user user.c
+	gcc -o worker worker.c
---
+++ user.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define READ 0
+#define WRITE 1
+
+int main() {
+	mkfifo("send2child", 0777);
+	mkfifo("send2parent", 0777);
+	int send2child = open("send2child", O_WRONLY | O_APPEND, 0777);
+	int send2parent = open("send2parent", O_RDONLY, 0777);
+	while (1) {
+		printf("Enter input: ");
+		char line[1000];
+		for (int i = 0; i < 1000; i++) line[i] = 0;
+		fgets(line, 1000, stdin);
+		write(send2child, line, strlen(line));
+		printf("Output: ");
+		for (int i = 0; i < 1000; i++) line[i] = 0;
+		read(send2parent, line, sizeof(line));
+		printf("%s\n", line);
+	}
+	close(send2child);
+	close(send2parent);
+	return 0;
+}
